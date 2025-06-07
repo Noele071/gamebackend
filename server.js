f8c71f808559be790c4348196baff5de48b405f5
Noele071 <126418744+Noele071@users.noreply.github.com>
2025-06-07 08:13:49 -0700

    server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
         'http://localhost:8080',
         'http://127.0.0.1:5500',
         'https://hypegame.netlify.app', 
-        'https://hypegamehub.xyz/',
+        'https://hypegamehub.xyz',
         'http://localhost:5500'
     ],
     credentials: true,
